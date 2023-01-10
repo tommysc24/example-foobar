38f95f7d8e95b93b7c69f76cc0cb7c6f5ea62fae
tommysc24 <120067591+tommysc24@users.noreply.github.com>
2023-01-10 16:24:18 -0500

    Update foo_bar_spec.rb

--- spec/foo_bar_spec.rb
+++ spec/foo_bar_spec.rb
@@ -1,4 +1,26 @@
 require "spec_helper"
 require_relative "../foo_bar"
 
-# TODO: Add FooBar specs
+class FooBar
+  def run(items = gets.chomp.to_i)
+    if items > 0
+      list = []
+      (1..items).each do |n|
+        if n % 3 == 0 && n % 5 == 0
+          list << "FOOBAR"
+        elsif n % 3 == 0
+          list << "FOO"
+        elsif n % 5 == 0
+          list << "BAR"
+        else
+          list << n
+        end
+      end
+    else
+      puts "Please enter a positive number"
+    end
+
+    list
+  end
+end
+
