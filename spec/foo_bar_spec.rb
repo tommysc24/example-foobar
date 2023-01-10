98d762e513e1f9e079602c0aa1075312aa6b16f0
tommysc24 <120067591+tommysc24@users.noreply.github.com>
2023-01-10 16:52:12 -0500

    Update foo_bar_spec.rb

--- spec/foo_bar_spec.rb
+++ spec/foo_bar_spec.rb
@@ -1,26 +1,28 @@
+foo_bar_spec.rb
 require "spec_helper"
 require_relative "../foo_bar"
 
-class FooBar
-  def run(items = gets.chomp.to_i)
-    if items > 0
-      list = []
-      (1..items).each do |n|
-        if n % 3 == 0 && n % 5 == 0
-          list << "FOOBAR"
-        elsif n % 3 == 0
-          list << "FOO"
-        elsif n % 5 == 0
-          list << "BAR"
-        else
-          list << n
-        end
-      end
-    else
-      puts "Please enter a positive number"
-    end
+describe "FooBar" do
+  it "returns correct list of items" do
+    result = FooBar.new.run(15)
 
-    list
+    expect(result.length).to eq(15)
+    expect(result).to eq([
+      1,
+      2,
+      "FOO",
+      4,
+      "BAR",
+      "FOO",
+      7,
+      8,
+      "FOO",
+      "BAR",
+      11,
+      "FOO",
+      13,
+      14,
+      "FOOBAR",
+    ])
   end
 end
-
