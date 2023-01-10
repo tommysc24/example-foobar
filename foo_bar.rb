0a2ccd5543369bd074566947b67cb6fd8a7695ef
tommysc24 <120067591+tommysc24@users.noreply.github.com>
2023-01-10 16:20:28 -0500

    Update foo_bar.rb

--- foo_bar.rb
+++ foo_bar.rb
@@ -1,1 +1,22 @@
-# TODO: Add FooBar content
+class FooBar
+  def run(items = gets.chomp.to_i)
+    if items > 0
+      list = []
+      (1..items).each do |n|
+        if n % 3 == 0 && n % 5 == 0
+          list << "FOOBAR"
+        elsif n % 3 == 0
+          list << "FOO"
+        elsif n % 5 == 0
+          list << "BAR"
+        else
+          list << n
+        end
+      end
+    else
+      puts "Please enter a positive number"
+    end
+
+    list
+  end
+end
