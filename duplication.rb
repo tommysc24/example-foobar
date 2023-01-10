109c7938be8bc82e6757b59d3befd58e9d069535
tommysc24 <120067591+tommysc24@users.noreply.github.com>
2023-01-10 16:47:36 -0500

    Update duplication.rb

--- duplication.rb
+++ duplication.rb
@@ -1,6 +1,31 @@
-# TODO: Add duplication.rb content
-class Duplicated
-  def impl_one(a, b, c, d)
-    a + b + c + d / ( b * d / a) * (a / c) - (a * b * d * c * 100)
+
+require "securerandom"
+
+module DuplicationExample
+  def identical(message, header: true)
+    body = []
+    body << "-- HEADER --" if header
+    body << "-- MESSAGE --"
+    body << message
+    body << "-- FOOTER --"
+    message = body.join("\n")
+
+    if message.length > 100
+      message[0, 100]
+    else
+      message
+    end
+  end
+
+  def similar
+    hex = SecureRandom.hex(20)
+    hex.reverse
+    hex += "-suffix"
+
+    if hex.start_with?("z")
+      hex += "something-else"
+    end
+
+    hex
   end
 end
